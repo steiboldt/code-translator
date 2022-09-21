57f56f567d633e12207c6350aa029033f225a2bc
Marcus Steiboldt <ms227cn@student.lnu.se>
2022-09-21 12:55:29 +0200

    binary

--- modules/Translator/Translator.js
+++ modules/Translator/Translator.js
@@ -8,7 +8,49 @@ export class Translator {
     this.morseAlphabet = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", 
     ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
     "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+
+    this.binaryAlphabet = ['01000001', '01000010', '01000011', '01000100', '01000101', '01000110', '01000111', '01001000', '01001001', '01001010', '01001011', '01001100', '01001101', '01001110', '01001111', '01010000', '01010001', '01010010', '01010011', '01010100', '01010101', '01010110', '01010111', '01011000', '01011001', '01011010']
   }
+
+  binaryToEnglish(text) {
+    let textString = text
+    let stringArray = textString.split(' ')
+    let answer = ''
+
+    for (let i = 0; i < stringArray.length; i++) {
+      for (let j = 0; j < this.binaryAlphabet.length; j++) {
+        if (this.binaryAlphabet[j] === stringArray[i]) {
+          answer = answer + this.englishAlphabet[j]
+        }
+      }
+      
+    }
+    return answer
+  }
+
+  /**
+   * Method for translating text to binary. 
+   *
+   * @param {string} text String to translate
+   * @returns string translated into binary code. 
+   */
+  englishToBinary(text) {
+    let textString = text.toLowerCase()
+    let stringArray = textString.split('')
+    let answer = ''
+
+    for (let i = 0; i < stringArray.length; i++) {
+      for (let j = 0; j < this.englishAlphabet.length; j++) {
+        if (this.englishAlphabet[j] === stringArray[i])  {
+          answer = answer + this.binaryAlphabet[j] + ' '
+        }
+        
+      }
+      
+    }
+    return answer
+  }
+
 
   /**
    * Method for translating a string (a-z) into morsecode.
@@ -20,6 +62,13 @@ export class Translator {
     let textString = text.toLowerCase()
     let stringArray = textString.split('')
     let answer = ''
+
+    /* stringArray.forEach(letter => {
+      if (!this.englishAlphabet.includes(letter)) {
+        throw new Error('Error: This is not a valid input, only a-z')
+        
+      }
+    }) */
 
     for (let i = 0; i < stringArray.length; i++) {
       for (let j = 0; j < this.englishAlphabet.length; j++) {
@@ -37,7 +86,7 @@ export class Translator {
    * Method for translating morse into english. 
    *
    * @param {string} text the string to translate.
-   * @returns String translated into english from morrsecode.
+   * @returns String translated into english from morsecode.
    */
   morseToEnglish(text) {
     let textString = text
--- test-app.js
+++ test-app.js
@@ -2,13 +2,17 @@ import { Translator } from './modules/Translator/Translator.js'
 
 // Test app to display methods in the module
 //
-//
+// !! Note that this module only works with characters a-z !!
 
 const translator = new Translator()
 
 let englishToMorseString = 'This will be translated into morse'
 let morseToEnglishString = '- .... .. ... .-- .. .-.. .-.. -... . - .-. .- -. ... .-.. .- - . -.. .. -. - --- . -. --. .-.. .. ... ....' //thiswillbetranslatedintoenglish
+let englishToBinaryString = 'Hello'
+let binaryToEnglishString = '01001000 01000101 01001100 01001100 01001111'
 
 // Logging out the results.
 console.log(translator.englishToMorse(englishToMorseString))
 console.log(translator.morseToEnglish(morseToEnglishString))
+console.log(translator.englishToBinary(englishToBinaryString))
+console.log(translator.binaryToEnglish(binaryToEnglishString))
